cc734b12f0a3761967fdcf5ecf1d1aea75bc1390
JamalJohnson2 <150681547+JamalJohnson2@users.noreply.github.com>
2024-03-22 21:25:43 +0800

    ada
    adada

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
 
+  todoItem = new FormControl("");
+  todoList: string[] = []
+
+  pushItemToList() {
+
+    if (!this.todoItem.value){
+      alert("Please Enter Something huhuh");
+      return;
+    }
+
+    if( this.todoItem.valid){
+      this.todoList.push(this.todoItem.value as string);
+
+      this.todoItem.setValue('');
+    }
+    
+  }
 }
